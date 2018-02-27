3cde7c738d86477898b6a6b53c7a03d4649ccdbb
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2018-02-27 13:28:55 +0530

    uncommented

--- sqlitedb.py
+++ sqlitedb.py
@@ -4,27 +4,26 @@ import sqlite3
 
 conn = sqlite3.connect("testdb.db")                                   #connect DB, if not exist, new DB will be created
 
-# conn.execute('''create table stud
-#  	(roll_no int primary key not null,
-#  	name varchar(20),
-#  	father_name varchar(20) not null
-#  	);''')                                                            #create table
+conn.execute('''create table stud
+ 	(roll_no int primary key not null,
+ 	name varchar(20),
+ 	father_name varchar(20) not null
+ 	);''')                                                            #create table
 
-# conn.execute("insert into stud values(40, 'rkapoor', 'akapoor');")
-# conn.execute("insert into stud (father_name, roll_NO,name ) values ('ak', 081,NULL);")
-# conn.execute("insert into stud (name, FATher_name, roll_NO) values ('ak', 'ak', 0013);")
+conn.execute("insert into stud values(40, 'rkapoor', 'akapoor');")
+conn.execute("insert into stud (father_name, roll_NO,name ) values ('ak', 081,NULL);")
+conn.execute("insert into stud (name, FATher_name, roll_NO) values ('ak', 'ak', 0013);")
                                                                       # insert values
 
 cursor = conn.execute('''select * from stud;''')                      # select data
+for row in cursor:
+  print(row[0], row[1])                                             # only 0th and 1th column to display
 
-# for row in cursor:
-#   print(row[0], row[1])                                             # only 0th and 1th column to display
+cursor = conn.execute("select name from sqlite_master where type = 'table';")
+for i in cursor:
+	print(i[0])                                                       # get names of tables in current db
 
-# cursor = conn.execute("select name from sqlite_master where type = 'table';")
-# for i in cursor:
-# 	print(i[0])                                                       # get names of tables in current db
-
-# conn.execute("delete from stud where roll_NO = 4084;")              # deletion
+conn.execute("delete from stud where roll_NO = 4084;")                # deletion
 
 print(cursor.fetchone())
 print(cursor.fetchone())                                             # fetches current row, and point to next
@@ -42,6 +41,6 @@ conn.commit()                                                        # commit to
 print(conn.total_changes)                                            # returns no. of rows inserted, deleted, modified 
                                                                      # since DB connection was open
 
-# conn.rollback()                                                    # rollback chnages since last commit()
+conn.rollback()                                                      # rollback chnages since last commit()
 
 conn.close()                                                         # close connection
