92a6383df267f95e10cdc112d25b599643d4f8d6
Rakshit Kapoor <kapoor_rakshit@yahoo.co.in>
2019-05-22 13:30:12 +0530

    Update elementTree_XML_API.py

--- elementTree_XML_API.py
+++ elementTree_XML_API.py
@@ -6,7 +6,8 @@
 #                                   and Element represents a single node in this tree.
 
 
-
+# READING XML FILE
+---------------------
 # importing data by reading from a file
 import xml.etree.ElementTree as ET
 tree = ET.parse('testXML.xml')
@@ -51,8 +52,10 @@ for c in country_list:
 	print(c.get("capital"))
 	print("-----------------")
 
+	
 
-
+# MODIFYING EXISTING XML FILE
+-------------------------------
 # Element.text : changing its text content 
 # Element.set() : adding and modifying attributes
 # Element.remove() : remove it's child element, STRING not allowed, only Element object
@@ -80,3 +83,45 @@ tree.write('testXML.xml')
 # remove attribute of an element, using pop('key', defaultValue_to_return) as attrib is a dict()
 root[2][0].attrib.pop("updated", None)
 tree.write("testXML.xml")
+
+
+
+# CREATING NEW XML FILE
+------------------------
+# ELEMENT TREE
+tree = ET.ElementTree()
+
+
+# ELEMENT (tag) of TREE
+roottag = ET.Element("ROOT_TAG", {"version" : "1.0"})
+
+
+# SUB-ELEMENT (child tag)
+childtag1 = ET.SubElement(roottag, "CHILD_TAG_1")
+
+
+# attribute values can be configured one at a time with set()
+# or all at once by passing a dictionary
+childtag1.set("key1", "val1")
+childtag1.set("key1", "val modified")
+
+
+# COMMENT appended
+comment = ET.Comment('comment generated for ETwork example')
+childtag1.append(comment)
+
+
+# MODIFY text of ELEMENT
+childtag1.text = "content of child tag 1"
+
+
+# dict passed for attrib values
+childtag2 = ET.SubElement(childtag1, "CHILD_TAG_2", {"key1" : "val1", "key2" : "val2"})
+childtag2.text = "content of child tag 2"
+
+
+# SET root tag and write to a file
+tree._setroot(roottag)
+tree.write("chkxml.xml")
+
+
